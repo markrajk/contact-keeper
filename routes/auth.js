9bc574c0b83c45955423d2b46eef92b598be6326
markrajk <marko88rajkovic@gmail.com>
2019-09-27 20:19:01 +0200

    Jwt duration changed

--- routes/auth.js
+++ routes/auth.js
@@ -58,7 +58,7 @@ router.post(
         payload,
         config.get('jwtSecret'),
         {
-          expiresIn: 360000
+          expiresIn: 360
         },
         (err, token) => {
           if (err) throw err;
--- routes/users.js
+++ routes/users.js
@@ -54,7 +54,7 @@ router.post(
         payload,
         config.get('jwtSecret'),
         {
-          expiresIn: 360000
+          expiresIn: 360
         },
         (err, token) => {
           if (err) throw err;
